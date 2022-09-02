44e806fd2518f369a656933ed40e365bdc0c3290
kamcio420 <kamcio420@gmail.com>
2022-09-02 23:07:06 +0200

    Entry text

--- casino.cpp
+++ casino.cpp
@@ -1,6 +1,17 @@
 ﻿#include <iostream>
+#include <cwchar>
+#include <windows.h>
+
+using std::cout;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    system("Color 0A");
+    cout << "\n=======================================================================================================================================================";
+    cout << "\n BBBBB    UU   UU   LL       LL      IIIIIIIII    SSSSSSSSS   HH    HH             CCCCC       A        SSSSSSSSS   IIIIIIIII   NN     NN    OOOOOOOO     ";
+    cout << "\n B   B    UU   UU   LL       LL         III       SS          HH    HH           CC           A  A      SS             III      NN N   NN   OO      OO    ";
+    cout << "\n BBBBB    UU   UU   LL       LL         III       SSSSSSSSS   HHHHHHHH          CC           A    A     SSSSSSSSS      III      NN  N  NN   OO      OO    ";
+    cout << "\n B   B    UU   UU   LL       LL         III              SS   HH    HH           CC         AAAAAAAA           SS      III      NN   N NN   OO      OO    ";
+    cout << "\n BBBBBB    UUUUU    LLLLLL   LLLLLL  IIIIIIIII    SSSSSSSSS   HH    HH             CCCCC   A        A   SSSSSSSSS   IIIIIIIII   NN     NN    OOOOOOOO     ";
+    cout << "\n=======================================================================================================================================================\n";
 }
